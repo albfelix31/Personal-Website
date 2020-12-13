1cbf8db5cb208d5eb834573f63536787a3bdcbdb
albfelix31 <albert.felix31@gmail.com>
2020-12-13 11:05:39 -0500

    updated information

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,33 +9,37 @@ import ProfilePic from '../components/ProfilePic.png';
 export default class Homepage extends React.Component {
   render() {
     return (
-      <Container fluidclassName="main-container">
-        <h1>hi there! </h1>
-        <h2>Albert here :)</h2>
-        <Image alt="" src={ProfilePic} height="100" className="d-inline-block align-top" roundedCircle/>
-        <Row> 
-            <p> about me</p>
-        </Row>
-        <Row> 
-            <p> resume</p>
-        </Row>
-        <Row> 
-            <p> projects</p>
-        </Row>
-        <Row> 
-            <p> photography</p>
-        </Row>
-        <Row>
-            <Col> 
-                <p>linkedin</p>
-            </Col>
-            <Col> 
-                <p>github</p>
-            </Col>
-            <Col> 
-                <p>twitter</p>
-            </Col>
-        </Row>
+      <Container fluid className="main-container">
+        <h1>👋 hi there! </h1>
+        <div className="picture-bio-container">
+          <div>
+            <Image alt="" src={ProfilePic} height="100" className="d-inline-block align-top" roundedCircle/>
+          </div>
+          <div>
+            <p>i’m Albert, and i’m a senior at cuny–city college majoring in computer science and  minoiring in 
+              gender & sexuality studies.</p>
+            <p>in the past, i’ve interned at microsoft as a new technologist intern doing ios dev, and at twitter 
+              as a software engineering intern working on video ads. in the future, i aspire to use technology to 
+              help my communities.</p>
+            <p>outside tech, i also enjoy creating Spotify playlists, exploring thrift stores, and binge-watching 
+              anime!</p>
+            <p>let's connect :)</p>
+          </div>
+        </div>
+        <div className="btns-container">
+          <div>
+            <a href="https://www.linkedin.com/in/albertrfelix" class="button">linkedin</a>
+          </div>
+          <div>
+            <a href="https://www.github.com/albfelix31" class="button">github</a>
+          </div>
+          <div>
+            <a href="https://drive.google.com/file/d/1r5bdN2MqkovZf2d2gyCGk9qRx7IAnPVK/view?usp=sharing" class="button">resume</a>
+          </div>
+          <div>
+            <a href="https://www.twitter.com/artxbel" class="button">twitter</a>
+          </div>
+        </div>
       </Container>
     );
   }
