822f753c938d6cd82d346b5504cd818d77fa28d9
albfelix31 <albert.felix31@gmail.com>
2020-12-13 00:10:26 -0500

    created skeleton homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Row';
 import './Homepage.css';
-import ProfilePic from './ProfilePic.png';
+import ProfilePic from '../components/ProfilePic.png';
 
 export default class Homepage extends React.Component {
   render() {
     return (
-      <Container className="main-container">
-              <h1>Albert Felix!</h1>
-              <Image className="profile-pic" src={ProfilePic} roundedCircle/>
+      <Container fluidclassName="main-container">
+        <h1>hi there! </h1>
+        <h2>Albert here :)</h2>
+        <Image className="profile-pic" src={ProfilePic} fluid/>
+        <Row> 
+            <p> about me</p>
+        </Row>
+        <Row> 
+            <p> resume</p>
+        </Row>
+        <Row> 
+            <p> projects</p>
+        </Row>
+        <Row> 
+            <p> photography</p>
+        </Row>
+        <Row>
+            <Col> 
+                <p>linkedin</p>
+            </Col>
+            <Col> 
+                <p>github</p>
+            </Col>
+            <Col> 
+                <p>twitter</p>
+            </Col>
+        </Row>
       </Container>
     );
   }
