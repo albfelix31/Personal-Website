a7335f19a51bf43e22ff6169ee0733b8164e5982
albfelix31 <albert.felix31@gmail.com>
2020-12-13 18:20:44 -0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Home from './components/Homepage';
+import Homepage from './components/Homepage';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Container fluid>
-          <Row className="justify-content-center">
-            <Switch>
-              {/* HomePage */}
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </Row>
-        </Container>
-      </Router>
+      <body>
+        <Router>
+              <Switch>
+                {/* HomePage */}
+                <Route exact path="/" component={Homepage} />
+              </Switch>
+        </Router>
+      </body>
     );
   }
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,53 +1,84 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
-
 import './Homepage.css';
 import ProfilePic from '../components/ProfilePic.png';
 import Background from '../components/Background.jpg';
+import { faFilePdf} from "@fortawesome/free-solid-svg-icons";
+// import { faFileAlt, faFilePdf} from "@fortawesome/free-regular-svg-icons;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faTwitter, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
 
 export default class Homepage extends React.Component {
   render() {
     return (
-        <div class="main-container container p-5">
-          <h1>👋 hi there! </h1>
-          <div class="row justify-content-md-center">
-            <div class="col-5">
+      <body>
+        <div class="main-container justify-content-md-center">
+          <h1 class="salutations">👋 hi there! </h1>
+          <div class="profile-pic-bio-container">
+            <div className="profile-pic-container">
               <Image alt="" src={ProfilePic} className="profile-pic" roundedCircle/>
             </div>
-            <div class="col-5 bio">
-              <p>I’m Albert, and I’m a senior at CUNY-City College, majoring in Computer Science and  minoring in 
-                Gender & Sexuality Studies.</p>
-              <p>In the past, I’ve interned at Microsoft as a New Technologist intern doing iOS dev, and at Twitter 
-                as a Software Engineering intern working on video ads. In the future, I aspire to use technology to 
-                help my communities.</p>
-              <p>Outside tech, I also enjoy creating Spotify playlists, exploring thrift stores, and binge-watching 
-                anime!</p>
+            <div class="bio-container">
+              <p>I’m Albert, and I’m a student at CUNY-City College studying Computer Science and Gender & Sexuality Studies.</p>
+              <p>In the past, I’ve interned at Microsoft as a New Technologist intern doing iOS dev, and at Twitter as a Software 
+                Engineering intern working on video ads. In the future, I aspire to use technology to help my communities.</p>
+              <p>Outside tech, I also enjoy creating Spotify playlists, exploring thrift stores, and binge-watching anime!</p>
             </div>
           </div>
-          <div class="row justify-content-md-center links">
-            <div class="col-3">
+          <div class="links-container">
+            <div class="lets-connect-container">
               <h3>let's connect!</h3>
             </div>
-            <div class="col-1">
-              <div class="row">
-                <a href="https://www.linkedin.com/in/albertrfelix" rel="noreferrer" target="_blank" class="button">linkedin</a>
+            <div className="connect-buttons-container">
+              <div>
+                <div>
+                  <a href="https://www.linkedin.com/in/albertrfelix" rel="noreferrer" target="_blank" id="button" class="linkedin-button">
+                    <div class="icon">
+                      <FontAwesomeIcon icon={faLinkedin}/>
+                    </div>
+                    <div>
+                      <p>linkedin</p>
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="https://www.github.com/albfelix31" rel="noreferrer" target="_blank" id="button" class="github-button">
+                    <div class="icon">
+                      <FontAwesomeIcon icon={faGithub}/>
+                    </div>
+                    <div>
+                      <p>github</p>
+                    </div>
+                  </a>
+                </div>            
               </div>
-              <div class="row">
-                <a href="https://www.github.com/albfelix31" rel="noreferrer" target="_blank" class="button">github</a>
-              </div>            
+              <div>
+                <div >
+                  <a href="https://drive.google.com/file/d/1r5bdN2MqkovZf2d2gyCGk9qRx7IAnPVK/view?usp=sharing" rel="noreferrer" 
+                  target="_blank" id="button" class="resume-button">
+                    <div class="icon">
+                      <FontAwesomeIcon icon={faFilePdf}/>
+                    </div>
+                    <div>
+                      <p>resume</p>
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="https://www.twitter.com/artxbel" rel="noreferrer" target="_blank" id="button" class="twitter-button">
+                    <div class="icon">
+                      <FontAwesomeIcon icon={faTwitter}/>
+                    </div>
+                    <div>
+                      <p>twitter</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
-            <div class="col-1">
-              <div class="row">
-                <a href="https://drive.google.com/file/d/1r5bdN2MqkovZf2d2gyCGk9qRx7IAnPVK/view?usp=sharing" rel="noreferrer" 
-                target="_blank" class="button">resume</a>
-              </div>
-              <div class="row">
-                <a href="https://www.twitter.com/artxbel" rel="noreferrer" target="_blank" class="button">twitter</a>
-              </div>
-            </div>
-          </div>
+          </div> 
         </div>
+      </body>
     );
   }
 }
